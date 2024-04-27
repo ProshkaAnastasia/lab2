316cb4486b0a826491c89e9f38314210a05f8fa7
Irina <i@mail.ru>
2024-04-27 12:29:40 +0300

    Commit 1

--- K.java
+++ K.java
@@ -19,4 +19,8 @@ public class K implements D {
     public int ae() {
         return 9;
     }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
 }
